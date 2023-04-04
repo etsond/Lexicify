924c1c0abb6fa4430ac7ec0e8d69c885247cb892
Etson <etsond@ufl.edu>
2023-04-04 15:02:06 -0400

    added a condition if the user does not enter a word

--- main.js
+++ main.js
@@ -8,34 +8,42 @@ document.querySelector(".search-input").addEventListener("keydown", (event) => {
 
 function getWord() {
   let word = document.querySelector(".search-input").value;
-  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.length > 0) {
-        // showing the word in the DOM
-        document.querySelector(".word").innerText = word;
-        // showing the part of speech on the DOM
-        document.querySelector(".part-of-speech").innerText = data[0].meanings[0].partOfSpeech;
-        // showing the phonetic notation on the DOM
-        document.querySelector(".phonetic").innerText = data[0].phonetic || "";
-        // showing the definition on the DOM
-        document.querySelector(".definition").innerText = data[0].meanings[0].definitions[0].definition || "";
-        // showing example sentences using the word on the DOM
-        document.querySelector(".examples").innerText = data[0].meanings[0].definitions[0].example || "";
-        // empty error message
-        document.querySelector(".error-message").innerText = "";
-      } else {
+  if (word.trim() === "") {
+    // show error message
+    document.querySelector(".error-message").innerText = "Please enter a word to search";
+    // clear previous search results
+    empty();
+  } else {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length > 0) {
+          // showing the word in the DOM
+          document.querySelector(".word").innerText = word;
+          // showing the part of speech on the DOM
+          document.querySelector(".part-of-speech").innerText = data[0].meanings[0].partOfSpeech;
+          // showing the phonetic notation on the DOM
+          document.querySelector(".phonetic").innerText = data[0].phonetic || "";
+          // showing the definition on the DOM
+          document.querySelector(".definition").innerText = data[0].meanings[0].definitions[0].definition || "";
+          // showing example sentences using the word on the DOM
+          document.querySelector(".examples").innerText = data[0].meanings[0].definitions[0].example || "";
+          // empty error message
+          document.querySelector(".error-message").innerText = "";
+        } else {
+          empty();
+          let error = `Could not locate "${word}", please enter a new word`;
+          document.querySelector(".error-message").innerText = error;
+        }
+      })
+      .catch((err) => {
         empty();
-        let error = `Could not locate "${word}", please enter a new word`;
+        let error = `Could not locate "${word}", please enter a new word. Error message: ${err}`;
         document.querySelector(".error-message").innerText = error;
-      }
-    })
-    .catch((err) => {
-      empty();
-      let error = `Could not locate "${word}", please enter a new word. Error message: ${err}`;
-      document.querySelector(".error-message").innerText = error;
-    });
+      });
+  }
 }
+
 
 function empty() {
   document.querySelector(".word").innerText = "";
